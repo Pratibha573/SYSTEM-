7036750450a9068691203f391b68740ac46f0537
Pratibha573 <sypratibha63@gmail.com>
2025-11-19 10:21:07 +0530

    Refactor price calculation to use default values

--- cia2.py
+++ cia2.py
@@ -1,16 +1,20 @@
 import sys
+
+# If user does NOT give 3 parameters → use default values
 if len(sys.argv) != 4:
-    print("Usage: python item prices.py price1 price2 price3")
-    sys.exit()
-    
-    prices = float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])
-    
-    minimum_price = min(prices)
-    maximum_price = max(prices)     
-    average_price = sum(prices) / len(prices)
-    
-    #display results
-    print("minimun prices:",minimum_price)
-    print("maximum prices:",maximum_price)
-    print("average prices:",average_price)
-    
+    print("No parameters passed. Using default values...")
+    price1 = 10
+    price2 = 20
+    price3 = 30
+else:
+    price1 = float(sys.argv[1])
+    price2 = float(sys.argv[2])
+    price3 = float(sys.argv[3])
+
+minimum = min(price1, price2, price3)
+maximum = max(price1, price2, price3)
+average = (price1 + price2 + price3) / 3
+
+print("Minimum:", minimum)
+print("Maximum:", maximum)
+print("Average:", average)
