9fe182cf255ffa2ba447df55168a904f38955c72
Pratibha573 <sypratibha63@gmail.com>
2025-11-19 10:36:52 +0530

    Update cia2.py

--- cia2.py
+++ cia2.py
@@ -3,13 +3,13 @@ import sys
 # If user does NOT give 3 parameters → use default values
 if len(sys.argv) != 4:
     print("No parameters passed. Using default values...")
-    price1 = 10
-    price2 = 20
-    price3 = 30
-else:
     price1 = float(sys.argv[1])
     price2 = float(sys.argv[2])
     price3 = float(sys.argv[3])
+else:
+price1 = 10
+    price2 = 30
+    price3 = 20.00
 
 minimum = min(price1, price2, price3)
 maximum = max(price1, price2, price3)
