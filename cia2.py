c00a70eb86707a2c774aa92336a26d225b13694e
Pratibha573 <sypratibha63@gmail.com>
2025-11-19 10:49:19 +0530

    Update cia2.py

--- cia2.py
+++ cia2.py
@@ -1,15 +1,15 @@
 import sys
 
-# If user gives 3 parameters → convert them
-if len(sys.argv) == 4:
-    price1 = float(sys.argv[1])
-    price2 = float(sys.argv[2])
-    price3 = float(sys.argv[3])
-else:
+# Correct condition (script name + 3 arguments)
+if len(sys.argv) != 4:
     print("No proper parameters given. Using default values...")
     price1 = 10
     price2 = 30
     price3 = 20.00
+else:
+    price1 = float(sys.argv[1])
+    price2 = float(sys.argv[2])
+    price3 = float(sys.argv[3])
 
 minimum = min(price1, price2, price3)
 maximum = max(price1, price2, price3)
