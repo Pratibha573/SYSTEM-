51148db6c2d2ce908cd7fdbc45597a414f5cda4b
Pratibha573 <sypratibha63@gmail.com>
2025-11-19 10:54:15 +0530

    Update cia2.py

--- cia2.py
+++ cia2.py
@@ -2,9 +2,9 @@ import sys
 
 # Check if exactly 3 command-line arguments are given
 if len(sys.argv) == 4:
-    price1 = float(sys.argv[1])
-    price2 = float(sys.argv[2])
-    price3 = float(sys.argv[3])
+    price1 = 10
+    price2 = 30
+    price3 = 20.00
 else:
     print("No proper parameters given. Please enter values manually...")
     price1 = float(input("Enter price1: "))
